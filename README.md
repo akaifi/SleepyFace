b1da89254a23a361af981ef690d6964c508d365f
Ahmad A Kaifi <7kaifi@gmail.com>
2021-08-27 18:08:17 +0300

    REarrange the hole project

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 ![Alt text](https://raw.githubusercontent.com/akaifi/SleepyFace/main/assets/HachathonX/akChar.svg)
 # SleepyFace
 A quick and Hack-athony Game 
+
+
+
+
+# TEAM
+
+  Ahmad Kaifi @akaifi
+  Hora  @HoorAlbediwi
+  Sakinah
+  Faris Al-Faifi
+  Saud
+
+# THANKS
+
+  My team for not getting so annoyed that they hit me.
+
+# TECHNOLOGY COLOPHON
+
+  HTML5, CSS3, Web Audio, Web Starter Kit
---
+++ sw.js
@@ -0,0 +1,32 @@
+const version = "0.6.18";
+const cacheName = `sleepyface-${version}`;
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                    `/`,
+                    `/index.html`,
+                    `/styles/main.css`,
+                    `/scripts/main.min.js`,
+                    `/scripts/comlink.global.js`,
+                    `/scripts/messagechanneladapter.global.js`,
+                    `/scripts/pwacompat.min.js`,
+                ])
+                .then(() => self.skipWaiting());
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.open(cacheName)
+        .then(cache => cache.match(event.request, { ignoreSearch: true }))
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
