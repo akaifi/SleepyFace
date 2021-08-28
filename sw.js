80f843c85ca22b19090919968e1840a6da6a3252
Ahmad A Kaifi <7kaifi@gmail.com>
2021-08-28 11:09:24 +0300

    sw path fix reg error

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 const version = "0.6.18";
-const cacheName = `sleepyface-${version}`;
+const cacheName = `SleepyFace-${version}`;
 self.addEventListener('install', e => {
     e.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll([
-                    ``,
-                    `index.html`,
-                    `styles/main.css`,
-                    `scripts/main.min.js`,
-                    `scripts/comlink.global.js`,
-                    `scripts/messagechanneladapter.global.js`,
-                    `scripts/pwacompat.min.js`,
+                    `/SleepyFace/`,
+                    `/SleepyFace/index.html`,
+                    `/SleepyFace/styles/main.css`,
+                    `/SleepyFace/scripts/main.min.js`,
+                    `/SleepyFace/scripts/comlink.global.js`,
+                    `/SleepyFace/scripts/messagechanneladapter.global.js`,
+                    `/SleepyFace/scripts/pwacompat.min.js`,
                 ])
                 .then(() => self.skipWaiting());
         })
